147fca1cde40fc44eee5cc9901a8c2b7e3993385
TheXyberis <xyberis.work@gmail.com>
2026-03-19 00:05:33 +0100

    feat(console): add Insertion Sort and Selection Sort algorithms

--- 01_ConsoleApps/Sorting-Performance-Lab/Sorting-Performance-Lab/SortingAlgorithms/InsertionSort.cs
+++ 01_ConsoleApps/Sorting-Performance-Lab/Sorting-Performance-Lab/SortingAlgorithms/InsertionSort.cs
@@ -8,5 +8,20 @@ namespace Sorting_Performance_Lab.SortingAlgorithms
 {
     internal class InsertionSort
     {
+        public static void Sort(int[] array)
+        {
+            int n = array.Length;
+            for(int i = 1; i < n; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+                while(j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = key;
+            }
+        }
     }
 }
--- 01_ConsoleApps/Sorting-Performance-Lab/Sorting-Performance-Lab/SortingAlgorithms/SelectionSort.cs
+++ 01_ConsoleApps/Sorting-Performance-Lab/Sorting-Performance-Lab/SortingAlgorithms/SelectionSort.cs
@@ -8,5 +8,24 @@ namespace Sorting_Performance_Lab.SortingAlgorithms
 {
     internal class SelectionSort
     {
+        public static void Sort(int[] array)
+        {
+            int n = array.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; i++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                    int temp = array[i];
+                    array[i] = array[minIndex];
+                    array[minIndex] = temp;
+                }
+            }
+        }
     }
 }
