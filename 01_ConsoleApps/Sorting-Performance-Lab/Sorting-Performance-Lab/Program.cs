763e25086f89bcdf07f7d3a68c97c5b81a6be151
TheXyberis <xyberis.work@gmail.com>
2026-03-19 22:16:01 +0100

    feat(console): add Quick Sort algorithm

--- 01_ConsoleApps/Sorting-Performance-Lab/Sorting-Performance-Lab/Program.cs
+++ 01_ConsoleApps/Sorting-Performance-Lab/Sorting-Performance-Lab/Program.cs
@@ -12,6 +12,7 @@ namespace Sorting_Performance_Lab
         static void Main(string[] args)
         {
             //BubbleSort.Sort(array);
+            //QuickSort.Sort(array, 0, array.Length - 1);
         }
     }
 }
--- 01_ConsoleApps/Sorting-Performance-Lab/Sorting-Performance-Lab/SortingAlgorithms/QuickSort.cs
+++ 01_ConsoleApps/Sorting-Performance-Lab/Sorting-Performance-Lab/SortingAlgorithms/QuickSort.cs
@@ -8,5 +8,38 @@ namespace Sorting_Performance_Lab.SortingAlgorithms
 {
     internal class QuickSort
     {
+        static void Sort(int[] array, int left, int right)
+        {
+            int pivot = array[(left + right) / 2];
+            int i = left;
+            int j = right;
+            while (i < j)
+            {
+                while (array[i] < pivot)
+                {
+                    i++;
+                }
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }           
+            }
+            if (left < j)
+            {
+                Sort(array, left, j);
+            }
+            if (i < right)
+            {
+                Sort(array, i, right);
+            }
+        }
     }
 }
