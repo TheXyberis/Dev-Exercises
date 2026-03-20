fc5670b588284b670f2823c19b7c8a3b89b80deb
TheXyberis <xyberis.work@gmail.com>
2026-03-21 00:41:16 +0100

    feat(console): add Book model and sample data for Advanced-Search-Engine project

---
+++ 01_ConsoleApps/Advanced-Search-Engine/Advanced-Search-Engine/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Search_Engine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 01_ConsoleApps/Advanced-Search-Engine/Advanced-Search-Engine/SampleData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Advanced_Search_Engine.Models;
+
+namespace Advanced_Search_Engine
+{
+    public static class SampleData
+    {
+        public static List<Book> Books = new List<Book>
+        {
+            new Book{Title = "Clean Code", Author = "Robert C. Martin"},
+            new Book{ Title = "The Pragmatic Programmer", Author = "Andrew Hunt" },
+            new Book { Title = "Algorithms Unlocked", Author = "Thomas H. Cormen" },
+            new Book { Title = "C# in Depth", Author = "Jon Skeet" },
+            new Book { Title = "Design Patterns", Author = "Erich Gamma" }
+        };
+    }
+}
