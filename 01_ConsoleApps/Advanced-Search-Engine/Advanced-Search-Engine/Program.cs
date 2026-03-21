d1623afc4b0ef44eea0dd45302562675a6172a16
TheXyberis <xyberis.work@gmail.com>
2026-03-21 22:49:35 +0100

    feat(console): add manual sorting logic for Book collection

--- 01_ConsoleApps/Advanced-Search-Engine/Advanced-Search-Engine/Program.cs
+++ 01_ConsoleApps/Advanced-Search-Engine/Advanced-Search-Engine/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Advanced_Search_Engine.Models;
+using Advanced_Search_Engine.SearchAlgorithms;
 
 namespace Advanced_Search_Engine
 {
@@ -10,6 +12,43 @@ namespace Advanced_Search_Engine
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Advanced Search Enging Demo");
+            Console.WriteLine(new string('-', 40));
+
+            List<Book> books = SampleData.Books;
+
+            Console.WriteLine("Original list of books: ");
+            DisplayBooks(books);
+
+            SortBooks(books);
+            Console.WriteLine("\nBooks sorted by title");
+            DisplayBooks(books);
+        }
+
+        public static void SortBooks(List<Book> books)
+        {
+            int n = books.Count;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    //compare the first and the second book
+                    if (books[j].Title.CompareTo(books[j + 1].Title) > 0)
+                    {
+                        Book temp = books[j];
+                        books[j] = books[j + 1];
+                        books[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        static void DisplayBooks(List<Book> books)
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine($" - {book}");
+            }
         }
     }
 }
